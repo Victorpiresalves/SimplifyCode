c8a3dda98dfb8b5db58b8b93b71be5dbc9a4e36e
VictorPiresAlves <victorpiresalves@hotmail.com>
2020-03-09 21:18:25 -0300

    first coomit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SimplifyCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var listaDeArquivos = "";
+
+            var baseArquivo = File.ReadAllText("C:\\Users\\Victor Pires\\Source\\Repos\\SimplifyCode\\SimplifyCode\\arquivos\\arquivoGenerico.txt", Encoding.UTF7);
+
+
+
+            var controllers =
+                              "CardapioFranquiaController," +
+                              "CategoriaFranquiaController," +
+                              "ClienteController," +
+                              "FranquiaController," +
+                              "HomeController";
+
+            var split = controllers.Split(",");
+
+
+            for (int i = 0; i < split.Length; i++)
+            {
+                listaDeArquivos += baseArquivo.Replace("_classe", split[i]);
+            }
+
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
